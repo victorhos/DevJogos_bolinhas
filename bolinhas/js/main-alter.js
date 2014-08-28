61614e91232671ab313cce2a1f53a1a18dfae28e
Victor Hugo <victor.hos@gmail.com>
2014-08-28 16:21:05 -0300

    add gitignore and line balls

--- bolinhas/js/main-alter.js
+++ bolinhas/js/main-alter.js
@@ -1,28 +1,30 @@
 
-	var game = new Phaser.Game(
-								800, 
-								600, 
-								Phaser.CANVAS, 
-								'phaser-example', 
-								{ preload: preload, 
-								  create: create, 
-								  update: update, 
-								  render: render }
-	);
+    var game = new Phaser.Game(
+                                800, 
+                                600, 
+                                Phaser.CANVAS, 
+                                'phaser-example', 
+                                { preload: preload, 
+                                create: create, 
+                                update: update, 
+                                render: render }
+    );
 
-	function preload() {
+    function preload() {
 
-		game.load.spritesheet('balls', 'img/balls.png', 17, 17);
+        game.load.spritesheet('balls', 'img/balls.png', 17, 17);
 
-	}
+    }
 
 	//Criando variaveis auxiliares
-	var list_line = {};
-	var list_ball = {};
-  var qtd = undefined;
+    var list_line = {};
+    var list_ball = {};
+    var list_intersects = {};
+    var qtd = undefined;
+    var c = 'rgb(255,255,255)';
 
-  //Funcao que cria as bolinhas
-	function createElements(qtd_elements){
+    //Funcao que cria as bolinhas
+    function createElements(qtd_elements){
         
         qtd = qtd_elements;
 
@@ -36,75 +38,100 @@
 
 	function create() {
 
-		game.stage.backgroundColor = '#124184';
-		createElements(8);
-		qtd = 8;
+        game.stage.backgroundColor = '#124184';
+        createElements(8);
+        qtd = 8;
 
-		//colocando as bolinhas na tela
-		for (var i = 0; i <= qtd; i++){
-		
-			list_ball['bolinha' + i].anchor.set(0.5);
-			list_ball['bolinha' + i].inputEnabled = true;
-			list_ball['bolinha' + i].input.enableDrag(true);
+        //colocando as bolinhas na tela
+        for (var i = 0; i <= qtd; i++){
 
-		}
+        list_ball['bolinha' + i].anchor.set(0.5);
+        list_ball['bolinha' + i].inputEnabled = true;
+        list_ball['bolinha' + i].input.enableDrag(true);
 
-		//Criando as linhas
-		for (var i = 0; i <= qtd; i++){
-			if ( i < qtd){
-            	list_line['line' + i] = new Phaser.Line(list_ball['bolinha' + i].x, 
-													    												list_ball['bolinha' + i].y, 
-                              						    				list_ball['bolinha' + (i + 1)].x, 
-													    												list_ball['bolinha' + (i + 1)].y);
-			}
-			else{
-            	list_line['line' + i]= new Phaser.Line(	list_ball['bolinha' + i].x, 
-													   													list_ball['bolinha' + i].y, 
-                              						  				 	list_ball['bolinha0'].x, 
-													   													list_ball['bolinha0'].y);
-			}
-		}
+        }
 
-		//escrevendo as linhas na tela
-		for (var i = 0; i <= qtd; i++){
+        //Criando as linhas
+        for (var i = 0; i <= qtd; i++){
 
-			if ( i < qtd){
+            if ( i < qtd){
 
-            	list_line['line' + i].fromSprite(	list_ball['bolinha' + i], 
-																							 	list_ball['bolinha' + (i + 1)], 
-																 								false);
-									}
-			else{
+                list_line['line' + i] = new Phaser.Line(
+                                            list_ball['bolinha' + i].x, 
+                                            list_ball['bolinha' + i].y, 
+                                            list_ball['bolinha' + (i + 1)].x, 
+                                            list_ball['bolinha' + (i + 1)].y);
 
-            	list_line['line' + i].fromSprite(	list_ball['bolinha' + i], 
-												 												list_ball['bolinha0'], 
-												 												false);
-			}
-		}	
+            }
+            else{
 
+                list_line['line' + i]= new Phaser.Line(	
+                                            list_ball['bolinha' + i].x, 
+                                            list_ball['bolinha' + i].y, 
+                                            list_ball['bolinha0'].x, 
+                                            list_ball['bolinha0'].y);
+
+            }
+        }
+
+        //escrevendo as linhas na tela
+        for (var i = 0; i <= qtd; i++){
+
+            if ( i < qtd){
+
+                list_line['line' + i].fromSprite(
+                                            list_ball['bolinha' + i], 
+                                            list_ball['bolinha' + (i + 1)], 
+                                            false);
+            }
+            else{
+
+                list_line['line' + i].fromSprite(
+                                            list_ball['bolinha' + i], 
+                                            list_ball['bolinha0'], 
+                                            false);
+            }
+        }
 	}
 
-	function update() {
+    function update() {
 
-		for (var i = 0; i <= qtd; i++){
+        //create fromSprite
+        for (var i = 0; i <= qtd; i++){
 
-			if ( i < qtd){
+            if ( i < qtd){
 
-            	list_line['line' + i].fromSprite(	list_ball['bolinha' + i], 
-																							 	list_ball['bolinha' + (i + 1)], 
-																 								false);
-									}
-			else{
+                list_line['line' + i].fromSprite(
+                                        list_ball['bolinha' + i],
+                                        list_ball['bolinha' + (i + 1)], 
+                                        false);
+            }
+            else{
 
-            	list_line['line' + i].fromSprite(	list_ball['bolinha' + i], 
-												 												list_ball['bolinha0'], 
-												 												false);
-			}
-		}	 
-	}
+                list_line['line' + i].fromSprite(
+                                        list_ball['bolinha' + i], 
+                                        list_ball['bolinha0'], 
+                                        false);
+            }
+        }
 
-	function render() {
+        //create line intersects
 
-		game.debug.text("Drag the bolinhas", 32, 550);
 
-	}
+
+
+
+    }
+
+    function render() {
+
+        //create lines 
+        for (var i = 0; i <= qtd; i++){
+
+            game.debug.geom(list_line['line' + i], c);
+
+        }
+
+        game.debug.text("Drag the bolinhas", 32, 550);
+
+    }
